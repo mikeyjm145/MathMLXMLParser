005a07ab65b79977a94fdc6c9a77d53ac9bc722a
Michael Morguarge <mikeyjm14@gmail.com>
2015-11-08 02:28:46 -0500

    Fixed spelling errors...

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ module.exports = function parse(parser, input) {
 	var RegMath_To_MathML = 0;
 	var RegMath_To_MathML_Backup = 1;
 	var MathML_To_RegMath = 2;
-	
+
 	var output;
-	
+
 	if (input === null || input === undefined) {
 		console.log("Invalid data");
 	}
-	
+
 	if (parser <= 0 && parser >= 10) {
 		return "Error: Wrong range.";
 	}
-	
-	switch(parser) {
+
+	switch (parser) {
 		case RegMath_To_MathML:
 			output = regMathParser.parse(input);
 			break;
@@ -32,6 +32,6 @@ module.exports = function parse(parser, input) {
 			output = "coming soon!";
 			break;
 	};
-	
+
 	return output;
 };
